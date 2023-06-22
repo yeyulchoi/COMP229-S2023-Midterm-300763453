b5f258070840e5cc4f2150962aacffcd4c2fbfcf
Yeyul_Choi <yeyulchoi@outlook.com>
2023-06-22 02:24:08 -0400

    added Atlas connection

--- server/config/db.js
+++ server/config/db.js
@@ -1,4 +1,6 @@
 module.exports = {
   //local MongoDB deployment ->
-  "URI": "mongodb://localhost/books229"
+  //"URI": "mongodb://localhost/books229"
+  //altas 
+   "URI":"mongodb+srv://yeyulchoi:dWqrL8ScpkewLRCf@comp229-midtermtest.xldqkii.mongodb.net/books229"
 };
